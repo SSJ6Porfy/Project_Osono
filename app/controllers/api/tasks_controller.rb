e616d2cd0cb1b7f6232ea87112101e2a21ca5e1a
Porfirio Matias <ssj6porfy@hotmail.com>
2017-09-21 15:59:06 -0700

    added tasks controller

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,2 +1,50 @@
 class Api::TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      render :show
+    else
+      render json: @task.errors.full_messages, status: 422
+    end
+  end
+
+  def edit
+    @task = Task.find_by(params[:id])
+  end
+
+  def update
+    @task = current_user.tasks.find_by(params[:id])
+
+    if @task.update_attributes(task_params)
+      render :show
+    else
+      render json: @task.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @task = Task.find_by(params[:id])
+  end
+
+  def destroy
+    @task = Task.find_by(params[:id])
+    if @task
+      @task.destroy
+      render :show
+    else
+      render json: ["Task does not exist!"], status: 422
+    end
+  end
+
+  def task_params
+    params
+      .require(:task)
+      .permit(:project_id, :user_id, :name, :description)
+  end
 end
