d10ca39816fa614137c844dd59471c9b9799e5d3
Porfirio Matias <ssj6porfy@hotmail.com>
2017-09-23 13:15:56 -0700

    added styling to task index container and task item

--- frontend/components/osonoview/tasks/task_index.jsx
+++ frontend/components/osonoview/tasks/task_index.jsx
@@ -12,10 +12,13 @@ class TaskIndex extends React.Component {
 
   render() {
     let result = this.props.tasks.map((task, idx) => {
-      return <TaskIndexListItem key={idx+"porf"} task={task}/>
+      return <TaskIndexListItem className="task-index-item" key={idx+"porf"} task={task}/>
     })
     return (
       <div className="task-index-container">
+        <div className="task-index-header-container">
+          <a className="header-add-task-btn">Add Task</a>
+        </div>
         <ul>
           { result }
         </ul>
--- frontend/components/osonoview/tasks/task_index_list_item.jsx
+++ frontend/components/osonoview/tasks/task_index_list_item.jsx
@@ -6,9 +6,25 @@ class TaskIndexListItem extends React.Component {
     super(props)
   }
 
+  update(field) {
+    return (e) => {
+      this.setState({[field]: e.target.value})
+    }
+  }
+
   render() {
     return (
-      <li>{this.props.task.name}</li>
+      <div className="task-row">
+        <div className="item-row">
+          <li className="item-row-container">
+            <div className="task-status-checkbox-container">
+              <input className="task-status-checkbox" type="checkbox"/>
+            </div>
+            <textarea className="task-item-name-editable" value={this.props.task.name}/>
+          </li>
+        </div>
+      </div>
+
     )
   }
 }
