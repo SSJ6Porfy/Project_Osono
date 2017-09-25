22eb352a5a66b9f49d69978fb81f57abab4aa095
Porfirio Matias <ssj6porfy@hotmail.com>
2017-09-25 10:25:34 -0700

    added project api_utils and actions

---
+++ frontend/actions/project_actions.js
@@ -0,0 +1,53 @@
+import * as ProjectAPIUtil from "../util/project_api_util";
+
+export const RECEIVE_ALL_PROJECTS = 'RECEIVE_ALL_PROJECTS';
+export const RECEIVE_PROJECT = 'RECEIVE_PROJECT';
+export const REMOVE_PROJECT = 'REMOVE_PROJECT';
+export const RECEIVE_PROJECT_ERRORS = "RECEIVE_PROJECT_ERRORS";
+
+export const fetchProjects = () => dispatch => (
+  ProjectAPIUtil.fetchProjects()
+    .then(res => (dispatch(receiveAllProjects(res))
+  ), err => (dispatch(receiveProjectErrors(err.responseJSON))))
+);
+export const fetchProject = (id) => dispatch => (
+  ProjectAPIUtil.fetchProject(id)
+    .then(res => (dispatch(receiveProject(res))
+  ), err => (dispatch(receiveProjectErrors(err.responseJSON))))
+);
+export const createProject = (project) => dispatch => (
+  ProjectAPIUtil.createProject(project)
+    .then(res => (dispatch(receiveProject(res))
+  ), err => (dispatch(receiveProjectErrors(err.responseJSON))))
+);
+export const updateProject = (project) => dispatch => (
+  ProjectAPIUtil.updateProject(project)
+    .then(res => (dispatch(receiveProject(res))
+  ), err => (dispatch(receiveProjectErrors(err.responseJSON))))
+);
+
+export const deleteProject = (id) => dispatch => (
+  ProjectAPIUtil.deleteProject(id)
+    .then(res => (dispatch(removeProject(res))
+  ), err => (dispatch(receiveProjectErrors(err.responseJSON))))
+);
+
+
+export const receiveAllProjects = (projects) => ({
+  type: RECEIVE_ALL_PROJECTS,
+  projects
+})
+export const receiveProject = (project) => ({
+  type: RECEIVE_PROJECT,
+  project
+})
+
+export const removeProject = (project) => ({
+  type: REMOVE_PROJECT,
+  project
+})
+
+export const receiveProjectErrors = (errors) => ({
+  type: RECEIVE_PROJECT_ERRORS,
+  errors
+});
