efaeb0aaa70e096dd9c2c3cc935b91740debad7d
Porfirio Matias <ssj6porfy@hotmail.com>
2017-09-26 17:01:20 -0700

    added form to team detail page

--- frontend/components/osonoview/teams/team_detail.jsx
+++ frontend/components/osonoview/teams/team_detail.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import ProjectIndexContainer from "../projects/project_index_container";
 
 class TeamDetail extends React.Component {
   constructor(props) {
     super(props)
   }
 
-  
-
   render() {
     return(
-      <div>
-        <h1>I'm the Team show Page</h1>
+      <div className="team-detail-container">
+        <div className="team-detail-form-container">
+          <form>
+            <input type="text" value={this.props.team.name}></input>
+            <textarea value={this.props.team.description} />
+            <input type="submit" value="Update Team"/>
+          </form>
+        </div>
+
+        <div className="team-project-list-container">
+          <ProjectIndexContainer/>
+        </div>
       </div>
     )
   }
