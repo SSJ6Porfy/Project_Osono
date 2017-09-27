8a9c6ea8c8df212a42943dd9d3b06b8a84fda9c2
Porfirio Matias <ssj6porfy@hotmail.com>
2017-09-26 17:30:57 -0700

    added updating functionality to team show page

--- frontend/components/osonoview/teams/team_detail.jsx
+++ frontend/components/osonoview/teams/team_detail.jsx
@@ -4,15 +4,42 @@ import ProjectIndexContainer from "../projects/project_index_container";
 class TeamDetail extends React.Component {
   constructor(props) {
     super(props)
+    this.state = this.props.team;
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchTeam(this.props.params.match.teamId);
+  }
+
+  update(field) {
+    return (e) => {
+      this.setState({[field]: e.target.value})
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.updateTeam(this.state)
+      .then(() => this.props.history.push(`/osonoview/teams/${this.props.team.id}`));
   }
 
   render() {
     return(
       <div className="team-detail-container">
         <div className="team-detail-form-container">
-          <form>
-            <input type="text" value={this.props.team.name}></input>
-            <textarea value={this.props.team.description} />
+          <form onSubmit={this.handleSubmit}>
+            <label>Team Name
+              <br/>
+              <input type="text"
+                     onChange={this.update("name")}
+                     value={this.state.name}></input>
+            </label>
+            <label>Team Mission
+              <br/>
+            <textarea onChange={this.update("description")}
+                        value={this.state.description} />
+            </label>
             <input type="submit" value="Update Team"/>
           </form>
         </div>
