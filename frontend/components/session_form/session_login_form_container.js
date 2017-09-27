fc86213eb47402939ff25ced6316b376a8db5b18
Porfirio Matias <ssj6porfy@hotmail.com>
2017-09-27 16:47:32 -0700

    added user_reducer for teammates and fetchTeammates callback upon successful login

--- frontend/components/session_form/session_login_form_container.js
+++ frontend/components/session_form/session_login_form_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import SessionLoginForm from "./session_login_form";
 import { login } from "../../actions/session_actions";
+import { fetchTeammates } from "../../actions/user_actions";
 
 const mapStateToProps = (state, ownProps) => {
   const signedIn = !!state.session.currentUser;
@@ -9,7 +10,8 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  login: (user) => dispatch(login(user))
+  login: (user) => dispatch(login(user)),
+  fetchTeammates: () => dispatch(fetchTeammates())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SessionLoginForm);
--- frontend/components/session_form/session_signup_form_container.js
+++ frontend/components/session_form/session_signup_form_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import SessionSignupForm from "./session_signup_form";
 import { signup } from "../../actions/session_actions";
+import { fetchTeammates } from "../../actions/user_actions";
 
 const mapStateToProps = (state, ownProps) => {
   const signedIn = !!state.session.currentUser;
@@ -9,7 +10,8 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  signup: (user) => dispatch(signup(user))
+  signup: (user) => dispatch(signup(user)),
+  fetchTeammates: () => dispatch(fetchTeammates())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SessionSignupForm);
