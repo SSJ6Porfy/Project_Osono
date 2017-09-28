4b486347c0978fb7667c5b7fb9d377c8fda1df08
Porfirio Matias <ssj6porfy@hotmail.com>
2017-09-28 00:07:12 -0700

    added functions to TeamMembers controller

--- app/controllers/api/team_members_controller.rb
+++ app/controllers/api/team_members_controller.rb
@@ -1,2 +1,31 @@
 class Api::TeamMembersController < ApplicationController
+
+  def index
+    @team_members = TeamMember.all
+  end
+
+  def show
+    @team_member = TeamMember.find_by(id: params[:id])
+  end
+
+  def destroy
+    @team_member = TeamMember.find_by(id: params[:id])
+    @team_member.destroy
+    render :show
+  end
+
+  def create
+    @team_member = TeamMember.new(team_member_params)
+
+    if @team_member.save
+      render :show
+    else
+      render json: @team_member.errors.full_messages, status: 422
+    end
+  end
+
+  def team_member_params
+    params.require(:team_member).permit(:user_id, :team_id)
+  end
+
 end
